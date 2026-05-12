fa42753f3d8fd269cc1d242cf90e578cb51cf7e7
annalikescoding <annalac2139@gmail.com>
2026-05-11 17:46:24 -0700

    Revert "Merge branch 'main' of https://github.com/annalikescoding/Minesweeper"
    This reverts commit b3ebf61f44ee682e212bfd25bc315b86688b39ca, reversing
    changes made to 3636ae3b2a016046acc14c0e65f0a84a1534ad97.

--- minesweeper/scripts/tile_map.gd
+++ minesweeper/scripts/tile_map.gd
@@ -1,11 +1,6 @@
 extends TileMap
 
-#Raynee:
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			var clicked_cell_coord = local_to_map(get_local_mouse_position())
-
-			print(clicked_cell_coord)
-			set_cell(0, clicked_cell_coord, -1)
-#hihihi
+			print("press tile")
